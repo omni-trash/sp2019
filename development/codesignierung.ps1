a76d2060e2ae75a33ae7452f58000d02c897ffc9
omni-trash <60014553+omni-trash@users.noreply.github.com>
2020-09-17 17:58:33 +0200

    Update codesignierung.ps1

--- development/codesignierung.ps1
+++ development/codesignierung.ps1
@@ -1,4 +1,3 @@
 # Code-Signierung, Ein entspr. Zertifikat sollte im Zertifikatsspeicher hinterlegt sein
-cd \temp
 $cert = Get-ChildItem Cert:\CurrentUser\My\ -CodeSigningCert
 Set-AuthenticodeSignature "C:\Pfad\zur\Datei" $cert -TimeStampServer "http://timestamp.globalsign.com/scripts/timstamp.dll"
